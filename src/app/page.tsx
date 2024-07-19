437b864a862163c3103fcb5eb2d272d2476ec4f6
Mrinal Mech <mrinalmech@gmail.com>
2024-07-19 08:11:05 +0530

    Read json file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      New
-    </main>
+import { promises as fs } from "fs";
+
+export default async function Home() {
+  const file = await fs.readFile(
+    process.cwd() + "/src/app/wage-data.json",
+    "utf8"
   );
+  const data = JSON.parse(file);
+
+  console.log(data);
+  return <main>New</main>;
 }
