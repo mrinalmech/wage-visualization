7cdf537854fda68ca151b4d90ae89a399977d47f
Mrinal Mech <mrinalmech@gmail.com>
2024-07-22 02:48:40 +0530

    Fix

--- src/app/ui/partials/WageVisualization/components/Graph/Graph.tsx
+++ src/app/ui/partials/WageVisualization/components/Graph/Graph.tsx
@@ -15,44 +15,30 @@ export default function Graph({
   selection,
   subSelection,
 }: Props) {
-  let content = null;
+  let header = "";
 
   if (selection === null) {
-    content = (
-      <p className="text-lg">
-        {`Please select ${
-          isVisualizationTypeState ? "a state" : "an occupation"
-        } from the dropdown on the left`}
-      </p>
-    );
+    header = `Please select ${
+      isVisualizationTypeState ? "a state" : "an occupation"
+    } from the dropdown on the left`;
   } else {
     const subSelectionArray = Object.values(subSelection);
 
     const subSelectionMade = subSelectionArray.reduce((a, c) => a || c, false);
 
     if (!subSelectionMade) {
-      content = (
-        <p className="text-lg">
-          {`Please select at least one ${
-            isVisualizationTypeState ? "occupation" : "state"
-          } from the checkboxes on the left`}
-        </p>
-      );
+      header = `Please select at least one ${
+        isVisualizationTypeState ? "occupation" : "state"
+      } from the checkboxes on the left`;
     } else {
-      content = <div>Graph</div>;
+      header = selection;
     }
   }
 
   return (
     <div className="p-4 flex-1 flex flex-col">
-      <h2
-        className={clsx("text-lg text-center", {
-          "opacity-0": selection === null,
-        })}
-      >
-        {selection || "Selection"}
-      </h2>
-      <div className="flex flex-1 items-center justify-center">{content}</div>
+      <h2 className="text-lg text-center mb-2">{header}</h2>
+      <div className="flex-1">Chart</div>
     </div>
   );
 }
