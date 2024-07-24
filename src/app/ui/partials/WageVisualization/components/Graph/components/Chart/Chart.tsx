32e89215bb549ab3f786432f58fa6df0675fd83f
Mrinal Mech <mrinalmech@gmail.com>
2024-07-24 07:22:45 +0530

    Add charts

--- src/app/ui/partials/WageVisualization/components/Graph/components/Chart/Chart.tsx
+++ src/app/ui/partials/WageVisualization/components/Graph/components/Chart/Chart.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 
 import { SubSelection, WageObject, WageTypes } from "@/interfaces";
@@ -43,15 +44,18 @@ const processWageData = (
       if (subSelection[o[secondarySelection]]) {
         if (data[o.year]) {
           if (wageTypes.nominal) {
-            data[o.year][`${o[secondarySelection]}-nominal`] = o.a_median;
+            data[o.year][`${o[secondarySelection]}-nominal`] =
+              o.a_median.toFixed(0);
           }
 
           if (wageTypes.rpp) {
-            data[o.year][`${o[secondarySelection]}-rpp`] = o.a_median_RPP;
+            data[o.year][`${o[secondarySelection]}-rpp`] =
+              o.a_median_RPP.toFixed(0);
           }
 
           if (wageTypes.cpi) {
-            data[o.year][`${o[secondarySelection]}-cpi`] = o.a_median_CPI;
+            data[o.year][`${o[secondarySelection]}-cpi`] =
+              o.a_median_CPI.toFixed(0);
           }
         } else {
           data = {
@@ -60,15 +64,18 @@ const processWageData = (
           };
 
           if (wageTypes.nominal) {
-            data[o.year][`${o[secondarySelection]}-nominal`] = o.a_median;
+            data[o.year][`${o[secondarySelection]}-nominal`] =
+              o.a_median.toFixed(0);
           }
 
           if (wageTypes.rpp) {
-            data[o.year][`${o[secondarySelection]}-rpp`] = o.a_median_RPP;
+            data[o.year][`${o[secondarySelection]}-rpp`] =
+              o.a_median_RPP.toFixed(0);
           }
 
           if (wageTypes.cpi) {
-            data[o.year][`${o[secondarySelection]}-cpi`] = o.a_median_CPI;
+            data[o.year][`${o[secondarySelection]}-cpi`] =
+              o.a_median_CPI.toFixed(0);
           }
         }
       }
@@ -97,22 +104,75 @@ export default function Chart({
     wageData
   );
 
+  let lines: ReactNode[] = [];
+
+  Object.entries(subSelection).forEach(([key, value]) => {
+    if (value) {
+      if (wageTypes.nominal) {
+        lines.push(
+          <Line
+            key={`${key}-nominal`}
+            type="monotone"
+            dataKey={`${key}-nominal`}
+            stroke="#8884d8"
+          />
+        );
+      }
+
+      if (wageTypes.rpp) {
+        lines.push(
+          <Line
+            key={`${key}-rpp`}
+            type="monotone"
+            dataKey={`${key}-rpp`}
+            stroke="#8884d8"
+            strokeDasharray="5 3"
+            dot={{ strokeDasharray: "0" }}
+          />
+        );
+      }
+
+      if (wageTypes.cpi) {
+        lines.push(
+          <Line
+            key={`${key}-cpi`}
+            type="monotone"
+            dataKey={`${key}-cpi`}
+            stroke="#8884d8"
+            strokeDasharray="1 2"
+            dot={{ strokeDasharray: "0" }}
+          />
+        );
+      }
+    }
+  });
+
   return (
     <LineChart
-      width={730}
-      height={500}
+      width={1000}
+      height={600}
       data={chartData}
       margin={{ top: 10, right: 30, left: 20, bottom: 5 }}
     >
       <XAxis
         dataKey="year"
         domain={[2017, 2022]}
-        ticks={[2017, 2018, 2019, 2020, 2021, 2022]}
+        ticks={[2018, 2019, 2020, 2021, 2022]}
         type="number"
       />
-      <YAxis type="number" domain={[0, 200000]} />
+      <YAxis
+        type="number"
+        domain={[0, 250000]}
+        ticks={[50000, 100000, 150000, 200000, 250000]}
+      />
       <Tooltip />
-      <Legend />
+      <Legend
+        align="right"
+        layout="vertical"
+        verticalAlign="middle"
+        wrapperStyle={{ paddingLeft: 50 }}
+      />
+      {lines}
     </LineChart>
   );
 }
