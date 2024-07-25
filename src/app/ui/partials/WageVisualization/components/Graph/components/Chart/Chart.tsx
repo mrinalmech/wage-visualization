4d3d5c76cb95212a51ae66346998bbe16f60cf66
Mrinal Mech <mrinalmech@gmail.com>
2024-07-26 03:54:43 +0530

    Legend

--- src/app/ui/partials/WageVisualization/components/Graph/components/Chart/Chart.tsx
+++ src/app/ui/partials/WageVisualization/components/Graph/components/Chart/Chart.tsx
@@ -64,6 +64,31 @@ const processWageData = (
   return chartData;
 };
 
+const BlankState = () => (
+  <div className="grid grid-cols-5 gap-4 w-full">
+    <div className="col-span-4">
+      <LineChart
+        width={800}
+        height={600}
+        data={[]}
+        margin={{ top: 10, right: 30, left: 20, bottom: 5 }}
+      >
+        <XAxis
+          dataKey="year"
+          domain={[2017, 2022]}
+          ticks={[2018, 2019, 2020, 2021, 2022]}
+          type="number"
+        />
+        <YAxis
+          type="number"
+          domain={[0, 150000]}
+          ticks={[50000, 100000, 150000]}
+        />
+      </LineChart>
+    </div>
+  </div>
+);
+
 export default function Chart({
   wageTypes,
   isVisualizationTypeState,
@@ -78,30 +103,7 @@ export default function Chart({
     selection === null || !subSelectionMade || !wageTypesSelected;
 
   if (renderBlankState) {
-    return (
-      <div className="grid grid-cols-5 gap-4 w-full">
-        <div className="col-span-4">
-          <LineChart
-            width={800}
-            height={600}
-            data={[]}
-            margin={{ top: 10, right: 30, left: 20, bottom: 5 }}
-          >
-            <XAxis
-              dataKey="year"
-              domain={[2017, 2022]}
-              ticks={[2018, 2019, 2020, 2021, 2022]}
-              type="number"
-            />
-            <YAxis
-              type="number"
-              domain={[0, 150000]}
-              ticks={[50000, 100000, 150000]}
-            />
-          </LineChart>
-        </div>
-      </div>
-    );
+    return <BlankState />;
   }
 
   const chartData = processWageData(
@@ -184,6 +186,7 @@ export default function Chart({
               key={`${key}-Nominal`}
               type="monotone"
               dataKey={`${key}-Nominal`}
+              name={key}
               stroke={color}
             />
           );
@@ -195,6 +198,7 @@ export default function Chart({
               key={`${key}-RPP`}
               type="monotone"
               dataKey={`${key}-RPP`}
+              name={key}
               stroke={color}
               strokeDasharray="5 3"
               dot={{ strokeDasharray: "0" }}
@@ -208,6 +212,7 @@ export default function Chart({
               key={`${key}-CPI`}
               type="monotone"
               dataKey={`${key}-CPI`}
+              name={key}
               stroke={color}
               strokeDasharray="1 2"
               dot={{ strokeDasharray: "0" }}
@@ -217,6 +222,94 @@ export default function Chart({
       }
     });
   }
+
+  const CustomTooltip = ({
+    active,
+    payload,
+    label,
+  }: {
+    active: boolean;
+    payload: any;
+    label: number;
+  }) => {
+    if (active && payload && payload.length) {
+      const tooltipItems: ReactNode[] = [];
+
+      Object.entries(subSelection).forEach(([key, value]) => {
+        if (value) {
+          let color = null as string | null;
+          let nominalValue = null as string | null;
+          let rppValue = null as string | null;
+          let cpiValue = null as string | null;
+
+          for (const elem of payload) {
+            if (elem.name === key) {
+              if (color === null) {
+                color = elem.color;
+              }
+
+              if (elem.dataKey === `${key}-Nominal`) {
+                nominalValue = elem.value;
+              } else if (elem.dataKey === `${key}-RPP`) {
+                rppValue = elem.value;
+              } else if (elem.dataKey === `${key}-CPI`) {
+                cpiValue = elem.value;
+              }
+
+              const { nominal, rpp, cpi } = wageTypes;
+
+              const loopFinished =
+                (!nominal || nominalValue !== null) &&
+                (!rpp || rppValue !== null) &&
+                (!cpi || cpiValue !== null);
+
+              if (loopFinished) {
+                break;
+              }
+            }
+          }
+
+          if (color) {
+            const nominalElem = nominalValue && (
+              <p style={{ color }} className="text-sm">
+                {`Nominal: $${nominalValue}`}
+              </p>
+            );
+
+            const RPPElem = rppValue && (
+              <p style={{ color }} className="text-sm">
+                {`RPP-adjusted: $${rppValue}`}
+              </p>
+            );
+
+            const CPIElem = cpiValue && (
+              <p style={{ color }} className="text-sm">
+                {`CPI-adjusted: $${cpiValue}`}
+              </p>
+            );
+
+            const elem = (
+              <div className="mb-2 last:mb-0" key={key}>
+                <p style={{ color }}>{key}</p>
+                {nominalElem}
+                {RPPElem}
+                {CPIElem}
+              </div>
+            );
+
+            tooltipItems.push(elem);
+          }
+        }
+      });
+      return (
+        <div className="bg-white p-3 border">
+          <p className="text-center">{label}</p>
+          {tooltipItems}
+        </div>
+      );
+    }
+    return null;
+  };
 
   return (
     <div className="grid grid-cols-5 gap-4 w-full">
@@ -234,7 +327,7 @@ export default function Chart({
             type="number"
           />
           <YAxis type="number" />
-          <Tooltip />
+          <Tooltip content={<CustomTooltip />} />
           {lines}
         </LineChart>
       </div>
